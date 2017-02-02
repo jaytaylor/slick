b6e9e1a302aafb015242329a96e58b25aa920e6c
Jay Taylor <outtatime@gmail.com>
2017-02-02 10:52:10 -0800

    Disabled broken webauth module.

--- example-bot/main.go
+++ example-bot/main.go
@@ -7,9 +7,9 @@ import (
 	"github.com/abourget/slick"
 	_ "github.com/abourget/slick/bugger"
 	_ "github.com/abourget/slick/deployer"
+	_ "github.com/abourget/slick/faceoff"
 	_ "github.com/abourget/slick/funny"
 	_ "github.com/abourget/slick/healthy"
-	_ "github.com/abourget/slick/faceoff"
 	_ "github.com/abourget/slick/hooker"
 	_ "github.com/abourget/slick/mooder"
 	_ "github.com/abourget/slick/plotberry"
@@ -17,10 +17,10 @@ import (
 	_ "github.com/abourget/slick/todo"
 	_ "github.com/abourget/slick/totw"
 	_ "github.com/abourget/slick/web"
-	_ "github.com/abourget/slick/webauth"
+	// _ "github.com/abourget/slick/webauth"
+	_ "github.com/abourget/slick/recognition"
 	_ "github.com/abourget/slick/webutils"
 	_ "github.com/abourget/slick/wicked"
-	_ "github.com/abourget/slick/recognition"
 )
 
 var configFile = flag.String("config", os.Getenv("HOME")+"/.slick.conf", "config file")
